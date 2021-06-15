5d5bdd24c80ecf57889f5881dfe69a61fd99fa0c
kevcorti <kevcorti@espol.edu.ec>
2021-06-15 00:29:44 -0500

    Codigo ejemplo

---
+++ TareaAspectos/src/Listener/EventListener.java
@@ -0,0 +1,8 @@
+package Listener;
+
+import java.io.File;
+
+public interface EventListener {
+	void update(String eventType, File file);
+
+}
--- TareaAspectos/src/tareaaspectos/TareaAspectos.java
+++ TareaAspectos/src/tareaaspectos/TareaAspectos.java
@@ -4,18 +4,27 @@
  * and open the template in the editor.
  */
 package tareaaspectos;
+import Editor.Editor;
+import Listener.EmailNotificationListener;
+import Listener.LogOpenListener;
 
-/**
- *
- * @author 59396
- */
+
+
+
+
 public class TareaAspectos {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+    	Editor editor = new Editor();
+        editor.events.subscribe("open", new LogOpenListener("/path/to/log/file.txt"));
+        editor.events.subscribe("save", new EmailNotificationListener("admin@example.com"));
+
+        try {
+            editor.openFile("test.txt");
+            editor.saveFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+  
     }
     
 }
